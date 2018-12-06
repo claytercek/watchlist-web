acee3a02e6402611dcf88c58dd53c32ea97de612
Clay Tercek <clay.tercek@gmail.com>
2018-12-06 18:13:35 -0500

    added swiped library

---
+++ Final/dist/js/app.js
@@ -0,0 +1,50 @@
+function routeTo(pathToPage) {
+	location.href = pathToPage;
+}
+
+function checkForAppUpdates() {
+	window.applicationCache.addEventListener("updateready", function(e) {
+		if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+			if (confirm("Updates are available for this mobile web app. Load them?")) {
+				window.applicationCache.swapCache();
+				window.location.reload();
+			}
+		}
+	});
+}
+
+var header = document.getElementsByTagName("header")[0];
+var arrow = document.getElementsByClassName("arrow")[0];
+var currentPageButton = document.querySelector(".current-page button");
+
+if (currentPageButton) {
+	currentPageButton.onclick = null;
+	currentPageButton.addEventListener("click", openMenu);
+	arrow.addEventListener("click", openMenu);
+}
+
+function openMenu() {
+	header.classList.toggle("maximized");
+}
+
+var watchListItems = document.getElementsByClassName("hasWatchData");
+
+function showData(selected) {
+	for (element of watchListItems) {
+		if (element != selected) {
+			element.classList.remove("showData");
+		} else {
+			element.classList.toggle("showData");
+		}
+	}
+}
+
+
+var swipe = Swiped.init({
+    query: 'main article',
+    list: true,
+    left: 80,
+	right: 80,
+	duration: 250,
+	tolerance: 80
+});
